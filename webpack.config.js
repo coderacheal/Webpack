fc10b214d871a0494f0e6f563985357f8c2ce12a
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-01-25 21:18:50 +0000

    Complete webpack exercise

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: './src/index.html',
+        }),
+    ],
+
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader']
+            },
+        ],
+    },
+
+    devServer: {
+        static: './dist',
+    },
+
+    optimization: {
+        runtimeCheck: 'single',
+    },
+    
+}
